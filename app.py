6966b948bd0345088652abd1b4b64f187144341e
Ashly <galvanperez@chapman.edu>
2025-05-04 15:41:43 -0700

    adding functions to app.py

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ from email.message import EmailMessage
 from datetime import datetime
 
 TOKENS = 'tokens.json'
+
+def load_token():
+    try:
+        with open(TOKENS, 'r') as token_file:
+            return json.load(token_file)
+    except FileNotFoundError:
+        return {}
+    
+def save_token(token):
+        with open(TOKENS, 'w') as token_file:
+            json.dump(token, token_file, indent=1)
