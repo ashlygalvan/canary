9d20c5057624360761cfedae5a7fadb6d81ef65f
Ashly <galvanperez@chapman.edu>
2025-05-04 14:03:10 -0700

    new folder and files

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print ("Hello World")
+from flask import Flask, request, render_template, redirect, url_for
+import uuid
+import json
+from email.message import EmailMessage
+from datetime import datetime
+
+TOKENS = 'tokens.json'
