9de0a7d0750da4c5b68b8db6bc7b69c73316e72a
Ashly <galvanperez@chapman.edu>
2025-05-04 16:12:44 -0700

    added defintions to app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 from email.message import EmailMessage
 from datetime import datetime
 
+app = Flask(__name__)
 TOKENS = 'tokens.json'
 
 def load_token():
@@ -16,3 +17,13 @@ def load_token():
 def save_token(token):
         with open(TOKENS, 'w') as token_file:
             json.dump(token, token_file, indent=1)
+
+def generate_token():
+     token_id = str(uuid.uuid4())
+     time = datetime.utcnow()
+     formatted_time = time.strftime('%Y-%m-%d %H:%M:%S')
+     token = load_token()
+     token[token_id] = {"created_at": formatted_time}
+     save_token(token)
+     print("Generated token: {}".format(token_id))
+     return token_id
