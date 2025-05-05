c104cecf8d5f88c3da9ea56a19d886c20f984556
Ashly <galvanperez@chapman.edu>
2025-05-04 21:45:54 -0700

    created the trigger definition

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import uuid
 import json
+import os
+import smtplib
 from email.message import EmailMessage
 from datetime import datetime
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 TOKENS = 'tokens.json'
+load_dotenv()
 
 def load_token():
     try:
@@ -33,6 +37,57 @@ def generate_token():
 @app.route('/')
 def index():
      return render_template("index.html")
+
+@app.route('/token/<token_id>')
+def trigger(token_id):
+     token = load_token()
+
+     #our error handling
+     if token_id not in token:
+          print("Not a valid token."), 404
+
+     user_email = token[token_id]['email']
+     triggered_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+     ip = request.remote_addr
+     user_agent = request.headers.get('User-Agent', 'Unknown')
+     token[token_id]['last_triggered'] = {
+        'timestamp': triggered_time,
+        'ip': ip,
+        'user_agent': user_agent
+    }
+     save_token(token)
+     try:
+        msg = EmailMessage()
+        msg['Subject'] = 'Canary Token was Triggered'
+        msg['From'] = f"Canary Alert <{user_email}>"
+
+        msg['To'] = user_email
+        msg.set_content(f"""
+We would like to notify you that your token was triggered!
+                        
+Token ID: {token_id}
+
+Time of trigger: {triggered_time}
+
+IP Address that triggered the token: {ip}
+
+From what user agent it came from: 
+
+{user_agent}
+
+If this is something that unexpectly done, review your systems.
+
+- Canary Token Notifications
+        """)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(os.getenv('EMAIL_USER'), os.getenv('EMAIL_PASS'))
+            smtp.send_message(msg)
+
+        return f"Token triggered! An alert was sent to {user_email}."
+     except Exception as e:
+        return f"Token triggered, but failed to send email: {str(e)}"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
